c60ce5b2a4c056d5fc20959d07254cb30c353be5
Dale Moore <daleamoore@gmail.com>
2022-09-27 21:12:07 +1000

    static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,9 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+      fallback: '200.html'
+		})
 	}
 };
 
